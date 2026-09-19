07480147c7941e5159034e9672410fd5e1762708
AbhishekBhovar <abhishekbhovar@gmail.com>
2026-09-19 21:56:28 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ function exportSave(){const blob=new Blob([JSON.stringify(state,null,2)],{type:'
 function finishSplash(){
   const splash=document.getElementById('startupSplash');
   if(!splash)return;
-  setTimeout(()=>splash.classList.add('leave'),2100);
-  setTimeout(()=>splash.remove(),2650);
+  setTimeout(()=>splash.classList.add('leave'),3000);
+  setTimeout(()=>splash.remove(),3360);
 }
 home();finishSplash();if('serviceWorker'in navigator)navigator.serviceWorker.register('sw.js');
