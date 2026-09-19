9aecb408846b56d1471db6e9139436d7ccfcacc0
AbhishekBhovar <abhishekbhovar@gmail.com>
2026-09-20 00:02:47 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="unburdened-v42-graph";
+const CACHE="unburdened-v44-fit";
 const FILES=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./assets/icon-192.png","./assets/icon-512.png","./assets/more-screen-journey.jpg","./assets/startup-journey.jpg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));
